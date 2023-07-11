e97c2cae86eccc9198a9b0f0f738731b26c345c5
Daadii <dadi1@speed.at>
2023-07-11 20:49:30 +0200

    reformatted code

--- Support.js
+++ Support.js
@@ -1,12 +1,11 @@
-
 // Calculate the radius in pixels
-function mm2px(valueMM){
+function mm2px(valueMM) {
 
-  // Get the PPI of the screen
-  const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
-  // Convert  mm to pixels
-  const valuePx = valueMM * (ppi / 25.4);
-  return valuePx;
+    // Get the PPI of the screen
+    const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
+    // Convert  mm to pixels
+    const valuePx = valueMM * (ppi / 25.4);
+    return valuePx;
 }
 
 
