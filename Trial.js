8ff9212811531ca642a242ad2bd7f67c61aea140
daadii <dadi1@speed.at>
2023-10-02 11:05:18 +0200

    added trialClockAngle to trial class

--- Trial.js
+++ Trial.js
@@ -1,12 +1,13 @@
 class Trial {
-    constructor(trialId, trialCategory, shape, trialDirection, intDevice, startIndex, targetIndex, startSize, targetWidth, targetHeight, amplitude) {
+    constructor(trialId, trialCategory, shape, trialDirection, trialClockAngle, intDevice, startIndex, targetIndex, startSize, targetWidth, targetHeight, amplitude) {
         this.trialId = trialId;                             // gives ID for each trial so that it can be tracked after shuffle
         this.trialCategory = trialCategory                  // shows the trial category string
         this.shape = shape;                                 // shows which shape will be used in the experiment e.g. "Rectangle" , "Circle"
         this.trialDirection = trialDirection;               // shows interaction direction e.g. "UP", "Down" , "Right" , "Left"
+        this.trialClockAngle = trialClockAngle;             // Determines the angle of the target object
         this.intDevice = intDevice;                         // shows the device used during the experiment "Mouse" , "Touch"  , "Laser Pointer"
-        this.startIndex = startIndex;
-        this.targetIndex = targetIndex;
+        this.startIndex = startIndex; // TODO marked for removal
+        this.targetIndex = targetIndex; // TODO marked for removal
         this.amplitude = amplitude;
         this.targetWidth = targetWidth;
         this.targetHeight = targetHeight;
