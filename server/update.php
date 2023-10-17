14721a732829197b639fd0f87f040984bca7a598
daadii <dadi1@speed.at>
2023-10-17 19:17:46 +0200

    reworked server script for better processing of trial data

--- server/update.php
+++ server/update.php
@@ -1,15 +1,23 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Allow requests from any origin (not recommended for production)
+header("Access-Control-Allow-Origin: *"); // Allows requests from any origin (not recommended for production)  TODO
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $postData = file_get_contents("php://input");
-    $file = 'data.csv';
+
+    // Determines accurate time for unique filename
+    date_default_timezone_set('Europe/Vienna'); // Sets the timezone to Austria (Europe/Vienna)
+    $timestamp = microtime(true);
+    $milliseconds = round(($timestamp - floor($timestamp))*100000);
+    $dateTime = date("Ymd_His", floor($timestamp));
+
+    $file = "./trialdata/data_{$dateTime}_{$milliseconds}.csv";
+
 
     if (file_put_contents($file, $postData)) {
-        echo 'File updated';
+        echo "[SERVER] File updated ($file)";
     } else {
-        echo 'Error writing to file';
+        echo '[SERVER] Error writing to file';
     }
 } else {
-    echo 'Invalid request';
+    echo '[SERVER] Invalid request - Only POST allowed';
 }
 ?>
