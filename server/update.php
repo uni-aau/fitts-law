74aa562d032ace0bfdbf532ca48d0703adf3bedb
daadii <dadi1@speed.at>
2023-10-17 19:37:40 +0200

    fixed file structure in server and reworked error handling

--- server/update.php
+++ server/update.php
@@ -1,21 +1,26 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Allows requests from any origin (not recommended for production)  TODO
+header("Access-Control-Allow-Origin: http://david.jamnig.net/tests/fittslaw"); // Allows requests from any origin (not recommended for production)  TODO
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $postData = file_get_contents("php://input");
 
     // Determines accurate time for unique filename
     date_default_timezone_set('Europe/Vienna'); // Sets the timezone to Austria (Europe/Vienna)
     $timestamp = microtime(true);
-    $milliseconds = round(($timestamp - floor($timestamp))*100000);
+    $milliseconds = round(($timestamp - floor($timestamp)) * 100000);
     $dateTime = date("Ymd_His", floor($timestamp));
 
-    $file = "./trialdata/data_{$dateTime}_{$milliseconds}.csv";
+    $file = "../trialdata/data_{$dateTime}_{$milliseconds}.csv";
 
 
     if (file_put_contents($file, $postData)) {
         echo "[SERVER] File updated ($file)";
     } else {
-        echo '[SERVER] Error writing to file';
+        $errorInfo = error_get_last();
+        if ($errorInfo !== null) {
+            echo "[SERVER] Error writing to file ($file). Error: " . $errorInfo['message'];
+        } else {
+            echo "[SERVER] Error writing to file ($file). No error information available.";
+        }
     }
 } else {
     echo '[SERVER] Invalid request - Only POST allowed';
