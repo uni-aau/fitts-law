cd061b314debf34548024f693ef82ee6016ede6a
daadii <dadi1@speed.at>
2023-10-19 20:22:02 +0200

    disabled dpi log

--- Support.js
+++ Support.js
@@ -2,7 +2,7 @@
 function mm2px(valueMM) {
     // Get the PPI of the screen
     const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
-    console.error("PPI: " + calcScreenDPI())
+    // console.error("PPI: " + calcScreenDPI())
     return valueMM * (ppi / 25.4); // Convert  mm to pixels
 }
 
