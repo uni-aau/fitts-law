b32252d282f1e879bdbc25f38f4f1c04b124239e
daadii <dadi1@speed.at>
2023-10-24 23:18:14 +0200

    reworked mm2px function but still not works lol

--- Support.js
+++ Support.js
@@ -1,57 +1,9 @@
 // Calculate the radius in pixels
 function mm2px(valueMM) {
     // Get the PPI of the screen
-    const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
-    // console.error("PPI: " + calcScreenDPI())
-    let value = valueMM/(25.4 / 216.333); // Convert  mm to pixels
-    console.error("Test = " + value);
+    // const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
+    // Hinweis - Zoom muss 100% sein!
+    const ppi = 551.44;
+    let value = (valueMM * ppi) / 25.4; // Convert  mm to pixels
     return value;
-}
-
-function calcScreenDPI() {
-    const el = document.createElement('div');
-    el.style = 'width: 1in;'
-    document.body.appendChild(el);
-    const dpi = el.offsetWidth;
-    document.body.removeChild(el);
-
-    return dpi;
-}
-
-function clacScreenDPI2() {
-    function getScreenDPI() {
-        // Create a hidden div element to measure physical screen size
-        var div = document.createElement('div');
-        div.style.width = '1in';
-        div.style.height = '1in';
-        div.style.position = 'absolute';
-        div.style.left = '-100%';
-        div.style.top = '-100%';
-      
-        // Add the div to the DOM
-        document.body.appendChild(div);
-      
-        // Measure the div's dimensions in pixels
-        var width = div.offsetWidth;
-        var height = div.offsetHeight;
-      
-        // Remove the div from the DOM
-        document.body.removeChild(div);
-      
-        // Calculate DPI based on screen resolution and physical size
-        var screenWidth = window.screen.width;
-        var screenHeight = window.screen.height;
-        var horizontalDPI = screenWidth / width;
-        var verticalDPI = screenHeight / height;
-      
-        return {
-          horizontalDPI: horizontalDPI,
-          verticalDPI: verticalDPI,
-        };
-      }
-      
-      var screenDPI = getScreenDPI();
-      console.log('Horizontal DPI:', screenDPI.horizontalDPI);
-      console.log('Vertical DPI:', screenDPI.verticalDPI);
-      
 }
