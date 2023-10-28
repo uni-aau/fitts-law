c6a2e49de1bb9aac62fe730c5b545e20746b5332
daadii <dadi1@speed.at>
2023-10-28 11:40:24 +0200

    reworked support function to display correct pixel amount

--- Support.js
+++ Support.js
@@ -1,9 +1,19 @@
-// Calculate the radius in pixels
+// Calculate the mm value in pixels to ensure that it is everywhere the same size
 function mm2px(valueMM) {
-    // Get the PPI of the screen
-    // const ppi = window.devicePixelRatio * 96; // assuming a default DPI of 96
+    const width = window.screen.width * window.devicePixelRatio;
+    const height = window.screen.height * window.devicePixelRatio;
+    const winWidth = window.innerWidth;
+    const winHeight = window.innerHeight;
+
+    // const totalCurrentTrialIndexEl = document.getElementById("totalCurrentTrialNumber");
+    // totalCurrentTrialIndexEl.textContent = `${winWidth} / ${winHeight}`;
+
+    console.log(`Width ${width} / ${winWidth} | Height ${height} / ${winHeight})`);
+
     // Hinweis - Zoom muss 100% sein!
-    const ppi = 551.44;
-    let value = (valueMM * ppi) / 25.4; // Convert  mm to pixels
-    return value;
+    const ppi = 151.32; // Convert  mm to pixels
+    return (valueMM * ppi) / 25.4;
 }
+
+// https://www.calculatorsoup.com/calculators/technology/ppi-calculator.php
+// Berechnung - Fullscreen -> winWidth, winHeight, dann Diagonale aus Internet und das dann in PPI eingeben
