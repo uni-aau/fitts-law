b81b475c863bd75d91a0f1b7e73b535acb75e0c9
daadii <dadi1@speed.at>
2023-12-28 18:22:41 +0100

    moved grid drawing to ElementDrawer.js

--- ElementDrawer.js
+++ ElementDrawer.js
@@ -78,4 +78,23 @@ class ElementDrawer {
         this.context.fillStyle = fillStyle;
     }
 
+    // Draws grid for debugging purposes
+    static drawGrid(canvasWidth, canvasHeight, context) {
+        const padding = 10; // Padding
+        const boxSizeX = 10;
+        const  boxSizeY = 10;
+
+        for (let i = 0; i <= canvasWidth; i += boxSizeX) {
+            context.moveTo(0.5 + i + padding, padding);
+            context.lineTo(0.5 + i + padding, canvasHeight + padding);
+        }
+
+        for (let i = 0; i <= canvasHeight; i += boxSizeY) {
+            context.moveTo(padding, 0.5 + i + padding);
+            context.lineTo(canvasWidth + padding, 0.5 + i + padding);
+        }
+        context.strokeStyle = "black";
+        context.stroke();
+    }
+
 }
--- STTrialHandling.js
+++ STTrialHandling.js
@@ -63,26 +63,7 @@ class STTrialHandling {
         this.targetElement = new ElementDrawer(context, this.targetCenterX, this.targetCenterY, this.targetWidthPx, this.targetHeightPx, this.shape);
         this.targetElement.drawTargetElement();
 
-        // Box width
-        // var bw = canvas.width;
-        // // Box height
-        // var bh = canvas.height;
-        // // Padding
-        // var p = 10;
-        // function drawBoard() {
-        //     for (var x = 0; x <= bw; x += 10) {
-        //         context.moveTo(0.5 + x + p, p);
-        //         context.lineTo(0.5 + x + p, bh + p);
-        //     }
-
-        //     for (var x = 0; x <= bh; x += 10) {
-        //         context.moveTo(p, 0.5 + x + p);
-        //         context.lineTo(bw + p, 0.5 + x + p);
-        //     }
-        //     context.strokeStyle = "black";
-        //     context.stroke();
-        // }
-        // drawBoard();
+        // ElementDrawer.drawGrid(canvas.width, canvas.height, context);
 
         // Determines which methods will be used to retrieve click position
         this.addClickListener();
